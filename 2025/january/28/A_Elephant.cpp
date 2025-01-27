bb803f53dbfd2489138431ec100dd98b682b43f3
engrsakib <snazmussakib01@gmail.com>
2025-01-28 02:38:31 +0600

    daily stack update

---
+++ 2025/january/28/A_Elephant.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define br "\n"
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+using namespace std;
+
+class Solution
+{
+public:
+    void solve()
+    {
+        
+    }
+};
+signed main()
+{
+    // _Created : 28 January 2025 ||  02:36:03
+    // _File    : A_Elephant.cpp
+    // Md. Nazmus Sakib
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    //اللهم صل على سيدنا محمدن النبى الامى وآله وسلم
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Solution obj;
+        obj.solve();
+    }
+    return 0;
+}
